28a33a1b509e2c89a498112f2876bc067024db75
Neenu <unni.neenu@gmail.com>
2026-06-02 18:43:15 +0530

    --Did assignment on fast api

--- main.py
+++ main.py
@@ -4,4 +4,34 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from your fresh FastAPI project!"}
+    return {"data" : {"name" : "Hello World"}}
+
+@app.get("/about")
+def about():
+    return {"data" : {"name" : "This is the about page"}}
+
+@app.get("/show/{id}")
+def show(id: int):
+    return {"data" : {"id" : id}}
+
+@app.get("/show/{id}/comments")
+def show_comments(id: int):
+    return {"data" : {"1", "2", "3"}}
+
+# Assignment 1
+
+@app.get("/books/search")
+def search(author : str, year : int = None, category : str = None):
+    result_data = {
+        "name": author,
+    }
+    if year is not None:
+        result_data["year"] = year
+    if category is not None:
+        result_data["category"] = category
+    return {"data" : result_data}
+
+@app.get("/books/{id}")
+def show_book(id : int):
+    return {"data" : {"id" : id}}
+
